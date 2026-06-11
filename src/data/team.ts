985da1d01bcaaca9e00af59a329c35a679572404
FarrasArias <rafael.arias.glez@gmail.com>
2026-06-11 15:06:27 -0700

    merge(staff) merged

--- src/data/team.ts
+++ src/data/team.ts
@@ -16,14 +16,14 @@ export const team: Person[] = [
     avatar: 'images/ramis_headshot.jpg',
     links: [{ label: 'LinkedIn', url: 'https://www.linkedin.com/in/ramis-lao/' }]
   },
-  /*{
+  {
     name: 'Rafael Arias',
-    role: 'Founder / Developer / Designer',
+    role: 'Founder / Developer',
       bio: 'I have 8+ years of development experience and 10+ years of teaching experience. I have worked and taught on Game Development, Data Science, Artificial Intelligence, Full-stack development, and Audio.',
     avatar: 'images/rafa_headshot.jpg',
-      cvUrl: 'documents/RafaelArias_CV_081125.pdf',
+      //cvUrl: 'documents/RafaelArias_CV_081125.pdf',
       links: [{ label: 'LinkedIn', url: 'https://www.linkedin.com/in/rafaelariasgonzalez-848528105/' }]
-    },*/
+    },
     {
         name: 'Sarah Grommelt',
         role: 'Founder / Developer',
